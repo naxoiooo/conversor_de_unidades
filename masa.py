0957e82c8bb4bd8d6fb8f8955a86bf1ed9ca2ac5
Naxu <ignacio.veliz@.alumnos.liceolaportada.cl>
2024-09-30 15:04:17 -0300

    Añadidos módulos de temperatura y masa

---
+++ masa.py
@@ -0,0 +1,11 @@
+def kilogramos_a_gramos(kg):
+    return kg * 1000
+
+def kilogramos_a_toneladas(kg):
+    return kg / 1000
+
+def gramos_a_kilogramos(g):
+    return g / 1000
+
+def toneladas_a_kilogramos(t):
+    return t * 1000
---
+++ temperatura.py
@@ -0,0 +1,17 @@
+def celsius_a_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def celsius_a_kelvin(celsius):
+    return celsius + 273.15
+
+def fahrenheit_a_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def fahrenheit_a_kelvin(fahrenheit):
+    return (fahrenheit - 32) * 5/9 + 273.15
+
+def kelvin_a_celsius(kelvin):
+    return kelvin - 273.15
+
+def kelvin_a_fahrenheit(kelvin):
+    return (kelvin - 273.15) * 9/5 + 32
